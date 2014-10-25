31f6296248335fd172df5e31a5855f7fea7dad5a
yohan marchiset <yohan.marchiset@gmail.com>
2014-10-25 15:33:19 +0200

    added gitignore file and src folder containing wififrames files

---
+++ src/wifiFrame.h
@@ -0,0 +1,28 @@
+#ifndef __WIFI_FRAME__
+#define __WIFI_FRAME__
+
+#include <stdint.h>
+
+/*
+	Frames types will be defined here
+*/
+#define TIME_FRAME 't'
+#define DISTANCE_FRAME 'd'
+#define DISCOVERY_FRAME 'D'
+
+/*
+	structure that will be sent as UDP data (our frame)
+*/
+typedef struct wifiFrame{
+	uint16_t seqNum;
+	char type;
+	uint32_t data[4];
+} wifiFrame;
+
+/*
+	Function for frame creation
+*/
+
+wifiFrame createWifiFrame(uint16_t seqNum, char type, uint32_t data1, uint32_t data2, uint32_t data3, uint32_t data4);
+
+#endif
